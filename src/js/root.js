2cd4fad1255e18091cbaaf481cbc35c913a65f41
egahqusayf <egahqusayf.d@gmail.com>
2023-04-10 02:17:13 +0700

    docs : root js Menambahkan file root.js pada folder src/js untuk react js, kemudian untuk mengcompilenya jalankan npm run build, hasil compile ada di folder public

---
+++ src/js/root.js
@@ -0,0 +1,12 @@
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+class App extends React.Component {
+    render(){
+        return(
+           <h1>ASRAMA YII</h1>
+        )
+    }
+}
+
+root.render(<App/>)
+console.log(2)
