7bd2bad22b1a82b5c7c8e99316d5daa1509ce990
egahqusayf <egahqusayf.d@gmail.com>
2023-04-26 09:56:55 +0700

    docs : react, tailwindcss, openAPI Menambahkan app react, menghubungkannya dengan tailwind dan membuat file 'myapi.json'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className=' text-8xl text-green-700'>Test tailwindcss</h1>
+    </div>
+  );
+}
+
+export default App;
