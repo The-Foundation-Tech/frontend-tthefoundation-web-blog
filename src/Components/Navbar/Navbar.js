0b3e054169332c408242a27422bf9e6b45a9a8cd
Fauzan <kaptozora@gmail.com>
2023-05-14 13:51:16 +0700

    add link nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -12,11 +12,18 @@ const Navbar = () => {
     <div className="flex justify-between items-center h-24 max-w-[1000px] mx-auto px-4 text-white">
       <h1 className="w-full text-3xl font-bold text-customRed text-left">Munashoroh.</h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Activities</li>
-        <li className="p-4">Contact</li>
+        <li className="p-4">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="p-4">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="p-4">
+          <Link to="/activities">Activities</Link>
+        </li>
+        <li className="p-4">
+          <Link to="/contact">Contact</Link>
+        </li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
@@ -24,10 +31,18 @@ const Navbar = () => {
       <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-customGray bg-customGray ease-in-out duration-500' : 'fixed left-[-100%]'}>
         <h1 className="w-full text-3xl font-bold text-customRed pt-3">Munashoroh.</h1>
         <ul className="uppercase p-4 text-customNavy">
-          <li className="p-4 border-b border-gray-600">Home</li>
-          <li className="p-4 border-b border-gray-600">About</li>
-          <li className="p-4 border-b border-gray-600">Activities</li>
-          <li className="p-4 border-b border-gray-600">Contact</li>
+          <li className="p-4 border-b border-gray-600">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="p-4 border-b border-gray-600">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="p-4 border-b border-gray-600">
+            <Link to="/activities">Activities</Link>
+          </li>
+          <li className="p-4 border-b border-gray-600">
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
       </div>
     </div>
