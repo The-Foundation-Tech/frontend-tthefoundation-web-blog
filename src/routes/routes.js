83cb6a22ae0e6c1e3788191e441118a57f9966fe
smnde <des.mn69@gmail.com>
2023-07-20 10:21:23 +0700

    menghapus modul admin

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "../views/layouts/MainLayout";
-import AdminLayout from "../views/layouts/AdminLayout";
-import Homepage from "../views/pages/Main/Homepage";
-import About from "../views/pages/Main/About";
-import Contact from "../views/pages/Main/Contact";
-import Dashboard from "../views/pages/Admin/Dashboard";
-import Activities from "../views/pages/Main/Activities";
+import Homepage from "../views/pages/Homepage";
+import About from "../views/pages/About";
+import Contact from "../views/pages/Contact";
+import Activities from "../views/pages/Activities";
 
 export default function Router() {
 	return (
@@ -17,11 +15,6 @@ export default function Router() {
 					<Route path="/about" element={<About />} />
 					<Route path="/contact" element={<Contact />} />
 					<Route path="/activities" element={<Activities />} />
-				</Route>
-
-				{/* Halaman yang menggunakan admin layout (admin panel) */}
-				<Route path="/admin" element={<AdminLayout />}>
-					<Route path="/admin/dashboard" element={<Dashboard />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
